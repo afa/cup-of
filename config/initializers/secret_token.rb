eb2a8b9cc262a06900874a41726de29af60f5453
afa <afa.alone@gmail.com>
2010-10-10 15:00:53 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CupOf::Application.config.secret_token = '6021f14099be4ce7bbfee13c3b3812849f91a8e859c087145cc764843e574f6a204bb45156356da040b46d3e4597c8dc81198548051a2bf90663e6089d3e7fd9'
