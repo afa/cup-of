4fcc4de9216fc63561ba108d801c1cf051849afd
afa <afa.alone@gmail.com>
2010-12-29 12:05:04 +0300

    main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,8 @@ class MainController < ApplicationController
   end
 
   def search
+   query = params[:search].andand[:q].andand.strip.andand.split(' ') || []
+   @searches = query.select{|q| q.mb_chars.strip.size >= 3 }.inject([]){|res, q| res + (Product.where("name ilike ?", "%#{q.mb_chars}%") + Product.where("description ilike ?", "%#{q.mb_chars}%"))}.uniq.paginate(:page=>params[:page] || 1, :per_page=>20)
   end
 
   def contacts
